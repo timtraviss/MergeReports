24ca62a5cf38279816a36331559035d077298bce
Tim Traviss <timtraviss@gmail.com>
2023-04-01 21:21:18 +1300

    delete columns

--- app.py
+++ app.py
@@ -39,48 +39,44 @@ if st.button('Start Merge', key='3'):
     # Convert the column to uppercase
     df1['QID'] = df1['QID'].str.upper() # Totara data 
     st.write('DF1 - QID transformed to uppercase')
+
     st.write(df1.head(2))
     # Add a new column to df2 and fill it with an Excel formula
     df2.insert(loc=2, column='QID', value=None)
-    st.write('DF2 - Column inserted')
+    # Extract the QID from the Trainee Column. 
     df2['QID'] = df2['Trainee'].str.extract(r'\((.*?)\)')
-    # df2['QID'].apply(lambda x: '=DATEDIF("' + str(x) + '", TODAY(), "M")')
-    # del df2['Trainee', 'Supervisors']
-    st.write(df2.head(2))
-    # df2['Months in DDP'] = df2[2].apply(lambda x: '=DATEDIF("' + str(x) + '", TODAY(), "M")')
-    # # Convert the Excel formula strings to actual formulas
-    # df2['Months in DDP'] = df2['Months in DDP'].apply(lambda x: None if x is None else x[1:] if x.startswith('=') else x)
-    # df2['Months in DDP'] = pd.to_numeric(df2['Months in DDP'], errors='coerce')
-
-    # # Convert the column to uppercase
-    # df2['QID'] = df2['QID'].str.upper() 
-
+    del df2['District']
+    del df2['Supervisors']
     # Merge the dataframes based on the common column
     merged_df = pd.merge(df1, df2, on='QID')
+    # Delete the column called 'Job Location'
+    del merged_df['Job Location']
+    del merged_df['Programme Suspended']
+    del merged_df['Date Suspended']
+    del merged_df['Leave Type']
+    del merged_df['Leave Return']
+    del merged_df['INV Other']
+    del merged_df['Wing number']
+    del merged_df['Trainee']
+    del merged_df['WEP Completed By']
+    del merged_df['WEP Completed Date']
+    del merged_df['WEP Moderation Details']
+    # Delete the Column called 'District_y'
+ 
+    # Rename the columns 
+    merged_df.rename(columns= {'District_x':'District', 'WEP Completion Percentage':'WEP (%)'})
+    ## Print Statements
+    print('renamed')
+    st.write('Merged_df')
     st.write(merged_df.head(5))
-    # def writer():
-    #     writer = pd.ExcelWriter('MergedReport.xlsx', engine='xlsxwriter')
-    #     merged_df.to_excel(writer, sheet_name='Report')
 
-    # def to_excel(merged_df):
-    #     output = BytesIO()
-    #     writer = pd.ExcelWriter(output, engine='xlsxwriter')
-    #     merged_df.to_excel(writer, index=False, sheet_name='Report')
-    #     workbook = writer.book
-    #     worksheet = writer.sheets['Report']
-    #     # format1 = workbook.add_format({'num_format': '0.00'}) 
-    #     # worksheet.set_column('A:A', None, format1)  
-    #     writer.save()
-    #     processed_data = output.getvalue()
-    #     return processed_data
-    # df_xlsx = to_excel(merged_df)
-    # st.download_button(label='📥 Download Current Result',
-    #                             data=writer,
-    #                             file_name= 'MergedReport.xlsx',
-    #                             key='4')
-
-    # merged_df.to_excel('NewReport.xlsx')
-    # print(merged_df)
-
+   
+    writer = pd.ExcelWriter('MergedReport.xlsx', engine='xlsxwriter')
+    merged_df.to_excel(writer, sheet_name='Report')
     st.success('All Merged')
+    st.download_button(label='📥 Download Current Result',
+                                data=writer,
+                                file_name= 'MergedReport.xlsx',
+                                key='4')    
+    
     print('ALL DONE!')
